3eeea6889b693f9531903ebeb714eef16fb9fb54
Marek Olszewski <marols@jetshop.se>
2020-04-11 20:13:26 +0200

    wip

--- dist/update.sh
+++ dist/update.sh
@@ -1,5 +1,14 @@
-wget -O app.zip https://github.com/molszews/lgtvapi/releases/latest/download/app.zip
+echo 'UPDATE!!!'
+echo `uptime` >update.log
+echo 'wget'
+wget -O app.zip https://github.com/molszews/lgtvapi/releases/latest/download/app.zip >>update.log
+echo 'unzip'
 unzip -p app.zip package.json >package.json
+echo 'unzip'
 unzip -p app.zip package-lock.json >package-lock.json
-npm install --production
-unzip -o app.zip
+echo 'npm'
+npm install --production >>update.log
+echo 'unzip'
+unzip -o app.zip >>update.log
+echo `uptime` >update.log
+echo 'DONE'
